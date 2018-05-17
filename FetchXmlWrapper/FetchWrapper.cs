6268582cc0a79f21f2373d45ca6ed534db0b747e
Michael Ochs <michael.ochs@cobalt.net>
2018-05-17 15:52:49 -0400

    Update for new fetch xml operators. Thanks to Aron Fischman.

--- FetchXmlWrapper/FetchWrapper.cs
+++ FetchXmlWrapper/FetchWrapper.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Collections;
 
-namespace FetchXmlTest
+namespace FetchXmlWrapper
 {
     [System.Serializable()]
     public class FetchWrapper
--- FetchXmlWrapper/Program.cs
+++ FetchXmlWrapper/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace FetchXmlTest
+namespace FetchXmlWrapper
 {
     class Program
     {
